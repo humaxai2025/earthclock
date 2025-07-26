8af17bf4a80d6b3db842a118283db38655c1cc69
iamsriram2025 <201474903+iamsriram2025@users.noreply.github.com>
2025-07-26 23:27:21 +0530

    configuration changes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-   
-  ],
-  define: {
-    'process.env': { ...process.env }
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // Disable sourcemaps for production
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 3000,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
   }
 })
